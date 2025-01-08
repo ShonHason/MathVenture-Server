2a3e471787a75587d897b6cd37b21c4edad52d97
ShonHason <ShonHason4@gmail.com>
2025-01-08 19:01:57 +0200

    add emails ability, routes,function, controller,modules and test

---
+++ src/routes/emailRoutes.ts
@@ -0,0 +1,10 @@
+import emailController from "../controllers/emailController";
+import express , { Request , Response , NextFunction } from "express";
+import { userTokensMiddleware } from "../controllers/userController";
+const router  = express.Router();
+
+router.post("/sendMail", userTokensMiddleware ,emailController.sendEmailAndSaveToDB);
+router.get("/getUserMail", userTokensMiddleware ,emailController.findEmailsByFilter);
+router.get("/getUserMail/:_id", userTokensMiddleware ,emailController.findEmailsByFilter);
+
+export default router;
